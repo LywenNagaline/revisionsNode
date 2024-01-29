4f8392d4f44e3e73bebe9e15bc6726161d9d707e
lywen <lywen.nagaline@gmail.com>
2024-01-29 17:35:18 +0100

    création du routeur

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-const Thing = require("./models/Thing");
+const stuffRoutes = require("./routes/stuff");
 
 const app = express();
 
@@ -33,41 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    //L'utilisation de new avec un modèle Mongoose crée par défaut un champ _id avec un identifiant unique généré automatiquement.
-    ...req.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.put("/api/stuff/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    // Nous sommes obligés de réattribuer _id car sinon nous tenterions de modifier un champ immuable dans un document de la BDD. Nous devons donc utiliser le paremètre id de la requête pour configurer notre Thing avec le même _id qu'avant.
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.delete("/api/stuff/:id", (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-app.get("/api/stuff", (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/stuff", stuffRoutes);
+//ici on importe les routes de stuff.js
 
 module.exports = app;
---
+++ backend/routes/stuff.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const Thing = require("../models/Thing");
+
+router.post("/", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    //L'utilisation de new avec un modèle Mongoose crée par défaut un champ _id avec un identifiant unique généré automatiquement.
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+router.put("/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    // Nous sommes obligés de réattribuer _id car sinon nous tenterions de modifier un champ immuable dans un document de la BDD. Nous devons donc utiliser le paremètre id de la requête pour configurer notre Thing avec le même _id qu'avant.
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+router.delete("/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+router.get("/:id", (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+router.get("/", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+module.exports = router;
